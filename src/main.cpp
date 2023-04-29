37eaef3053d8bddb83ef18e99c221b4789d9111d
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-04-29 12:09:22 +0200

    Fix issue in meta.lua generation

--- src/main.cpp
+++ src/main.cpp
@@ -779,12 +779,12 @@ void write_meta_data(const char* output_path)
 
     const char* meta_data_template =
         "return\n"
-        "(\n"
-        "    name            = \"%s\"\n"
-        "    path            = \"%s\"\n"
-        "    irradiance_size = %d\n"
-        "    prefilter_size  = %d\n"
-        "    brdf_lut_size   = %d\n"
+        "{\n"
+        "    name            = \"%s\",\n"
+        "    path            = \"%s\",\n"
+        "    irradiance_size = %d,\n"
+        "    prefilter_size  = %d,\n"
+        "    brdf_lut_size   = %d,\n"
         "}\n";
 
     char name_buffer[256];
